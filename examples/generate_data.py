d0e7fadfd5048bdc48b76fc85879cb8da2f8b863
Jasper Wouters <jasper.wouters@esat.kuleuven.be>
2018-11-06 11:31:01 +0100

    add four spikes to toy data of which two are overlapping and one is at the end of the file

--- examples/generate_data.py
+++ examples/generate_data.py
@@ -28,6 +28,7 @@ spike_size = 50 # in samples
 nb_channels = 30
 nb_channels_spike = 10 # number of channels on which a spike is measured
 data_dtype = 'float32'
+nb_spikes = 5
 
 # plot toy spike templates?
 plot = False
@@ -39,14 +40,16 @@ data = data.astype(data_dtype)
 
 # construct 2 clusters with each 3 "spikes"
 # CLUSTER 0
-cluster_0 = np.ones((3,2), dtype=np.int) * 0
+cluster_0 = np.ones((nb_spikes,2), dtype=np.int) * 0
 cluster_0[0,1] = 3000
 cluster_0[1,1] = 5000
 cluster_0[2,1] = 7000
+cluster_0[3,1] = 9000
+cluster_0[4,1] = 12400
 
 cluster_0_channel_template = np.linspace(-1, 0, spike_size)
 cluster_0_channel_scaling = np.linspace(0.5, 1.5, nb_channels_spike)
-cluster_0_spike_scaling = np.array([0.88, 1.05, 0.9])
+cluster_0_spike_scaling = np.array([0.88, 1.05, 0.9, 1.01, 1])
 
 cluster_0_template = np.dot(cluster_0_channel_scaling[:,None],
                             cluster_0_channel_template[None,:])
@@ -57,14 +60,16 @@ if plot:
     plt.title('template cluster 0')
 
 # CLUSTER 1
-cluster_1 = np.ones((3,2), dtype=np.int) * 1
+cluster_1 = np.ones((nb_spikes,2), dtype=np.int) * 1
 cluster_1[0,1] = 4000
 cluster_1[1,1] = 6000
 cluster_1[2,1] = 8000
+cluster_1[3,1] = 9010
+cluster_1[4,1] = 10000
 
 cluster_1_channel_template = np.linspace(0, -1, spike_size)
 cluster_1_channel_scaling = np.linspace(1.5, 0.5, nb_channels_spike)
-cluster_1_spike_scaling = np.array([1.3, 0.73, 1.12])
+cluster_1_spike_scaling = np.array([1.3, 0.73, 1.12, 0.8, 1])
 
 cluster_1_template = np.dot(cluster_1_channel_scaling[:,None],
                             cluster_1_channel_template[None,:])
@@ -74,8 +79,8 @@ if plot:
     plt.plot(cluster_1_template.T + np.linspace(0, 1, 10))
     plt.title('template cluster 1')
 
-# Insert 3 "spikes" for every cluster
-for idx in range(3):
+# Insert "spikes" for every cluster
+for idx in range(nb_spikes):
     # cluster 0
     data[:10,cluster_0[idx,1]:(cluster_0[idx,1]+spike_size)] = data[:10,cluster_0[idx,1]:(cluster_0[idx,1]+spike_size)] + cluster_0_template * cluster_0_spike_scaling[idx]
     data[-10:,cluster_1[idx,1]:(cluster_1[idx,1]+spike_size)] = data[-10:,cluster_1[idx,1]:(cluster_1[idx,1]+spike_size)] + cluster_1_template * cluster_1_spike_scaling[idx]
