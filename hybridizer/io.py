44e604860f6000f322c3b0bda004d512a016b7a8
Jasper Wouters <jasper.wouters@esat.kuleuven.be>
2019-04-03 16:29:03 +0200

    export hybrid data as column major by applying the transpose for sc compatibility

--- hybridizer/io.py
+++ hybridizer/io.py
@@ -95,7 +95,8 @@ class Recording:
     def save_raw(self, full_fn):
         """  Save the data in raw format in the original data folder
         """
-        self.data.tofile(full_fn)
+        # save transpose such that file is compatible with spyking circus
+        self.data.T.tofile(full_fn)
 
     def save_npy(self):
         """ Save the data in npy format in the original data folder
