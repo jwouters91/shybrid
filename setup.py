2fa7ab93572ed8ea2473d02b46cf3343b193c689
Jasper Wouters <jasper.wouters@esat.kuleuven.be>
2019-08-01 17:18:32 +0200

    doc changes

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='hybridizer',
-      version='0.1',
+      version='0.2',
       description='Hybrid Data Generator',
       author='Jasper Wouters',
       author_email='jasper.wouters@esat.kuleuven.be',
       packages=['hybridizer', 'hybridizer.ui'],
-      scripts=['shybride.py'],
+      scripts=['shybrid.py'],
      )
