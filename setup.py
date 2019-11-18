ca403d0e39ca2bfc166e287230905683f60bbf89
Jasper Wouters <jasper.wouters@esat.kuleuven.be>
2019-11-18 16:46:09 +0100

    move gui script inside the package and create entry point rather than script for windows compatibility

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='shybrid',
-      version='0.2.2',
+      version='0.2.3',
       description='A graphical tool for generating hybrid ground-truth spiking data for evaluating spike sorting performance',
       author='Jasper Wouters',
       author_email='jasper.wouters@esat.kuleuven.be',
@@ -14,7 +14,11 @@ setup(name='shybrid',
       long_description_content_type='text/markdown',
       url='https://github.com/jwouters91/shybrid',
       packages=['hybridizer', 'hybridizer.ui'],
-      scripts=['shybrid.py'],
+      entry_points={
+          'gui_scripts': [
+              'shybrid = hybridizer.shybrid:main'
+          ]
+      },
       classifiers=[
           'Programming Language :: Python :: 3',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
